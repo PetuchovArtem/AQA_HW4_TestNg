5d0ffea63056fc0c3c28e5393820b87c5e17d6d2
PetuchovArtem <petuchov.artem@gmail.com>
2021-08-07 16:23:45 +0300

    finish TestNG
    finish TestNG

--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -4,14 +4,15 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import triangle.Triangle;
 
+import java.text.DecimalFormat;
+import java.text.ParseException;
 import java.util.Scanner;
 
-import static java.lang.Integer.parseInt;
 
 public class TriangleTest {
     @DataProvider(name = "data-provider")
     public Object[][] dpMethod() {
-        return new Object[][]{{2, 2, 3}, {4, 5, 6}, {0, 5, 6}};
+        return new Object[][]{{2, 2, 3}, {4, 5, 6}, {2, 5, 6}};
     }
 
     @DataProvider(name = "data-provider2")
@@ -47,6 +48,20 @@ public class TriangleTest {
         return new Object[][]{{1, 2, 3}, {9, 6, 4}, {17, 22, 23}};
     }
 
+    @DataProvider(name = "data-provider8")
+    public Object[][] dpMethod8() {
+        return new Object[][]{{2, 3, 4, 2.9}, {1, 1, 1, 0.43}, {17, 19, 23, 158.64}};
+    }
+
+    @DataProvider(name = "data-provider9")
+    public Object[][] dpMethod9() {
+        return new Object[][]{{3.1, 3.2, 4.55, 4.96}, {1.09, 1.99, 1.001, 0.32}, {2.03, 2.04, 2.05, 1.8}};
+    }
+
+    @DataProvider(name = "data-provider10")
+    public Object[][] dpMethod10() {
+        return new Object[][]{{Math.pow(2, 50), Math.pow(2, 50), Math.pow(2, 50), 5.4890881146011916E29}};
+    }
 
     @Test(dataProvider = "data-provider", testName = "Сумма двух сторон треугольника, стороны больше нуля")
     public void testCheckTriangle(double arg0, double arg1, double arg2) {
@@ -104,4 +119,43 @@ public class TriangleTest {
         Triangle triangle = new Triangle(arg0, arg1, arg2);
         Assert.assertEquals(triangle.detectTriangle(), 4);
     }
+
+    @Test(dataProvider = "data-provider8", testName = "Площадь, целые числа")
+    public void testgetSquareZilie(double arg0, double arg1, double arg2, double arg3) throws ParseException {
+        Triangle triangle = new Triangle(arg0, arg1, arg2);
+        double squaze = triangle.getSquare();
+        DecimalFormat df = new DecimalFormat("0.00");
+        String formate = df.format(squaze);
+        double finalValue = (Double) df.parse(formate);
+        Assert.assertEquals(finalValue, arg3);
+    }
+
+    @Test(dataProvider = "data-provider9", testName = "Площадь, дробные числа числа")
+    public void testgetSquareDrob(double arg0, double arg1, double arg2, double arg3) throws ParseException {
+        Triangle triangle = new Triangle(arg0, arg1, arg2);
+        double squaze = triangle.getSquare();
+        DecimalFormat df = new DecimalFormat("0.00");
+        String formate = df.format(squaze);
+        double finalValue = (Double) df.parse(formate);
+        Assert.assertEquals(finalValue, arg3);
+    }
+
+    @Test(dataProvider = "data-provider10", testName = "Площадь, большая степень")
+    public void testgetSquareUpStepen(double arg0, double arg1, double arg2, double arg3) throws ParseException {
+        Triangle triangle = new Triangle(arg0, arg1, arg2);
+        double squaze = triangle.getSquare();
+        DecimalFormat df = new DecimalFormat("0.00");
+        String formate = df.format(squaze);
+        double finalValue = (Double) df.parse(formate);
+        Assert.assertEquals(finalValue, arg3);
+    }
+
+    @Test(expectedExceptions = NumberFormatException.class, testName = "Площадь, переполнение")
+    public void testgetSquarePerepolnenie() {
+        Scanner sc = new Scanner("text2.txt");
+        Triangle triangle = new Triangle(Integer.parseInt(sc.next()), Integer.parseInt(sc.next()), Integer.parseInt(sc.next()));
+        Assert.assertEquals(triangle.getSquare(), 1);
+    }
+
+
 }
